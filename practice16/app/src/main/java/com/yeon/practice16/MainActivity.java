6c32382c3ee6c8e5538c6996c50ff3e7d50ea66d
원성연 <yeon@wonseong-yeon-ui-MacBookAir.local>
2022-04-07 21:35:50 +0900

    도전 4 성공

---
+++ practice16/app/src/main/java/com/yeon/practice16/MainActivity.java
@@ -0,0 +1,76 @@
+package com.yeon.practice16;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_text;
+    private EditText et_message;
+    private String str;
+    long btnBack = 1;
+    private TextView tv_writing;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_text = findViewById(R.id.btn_text);
+        et_message = findViewById(R.id.et_message);
+        tv_writing = findViewById(R.id.tv_writing);
+
+
+
+        btn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = et_message.getText().toString();
+                Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        et_message.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                str = et_message.getText().toString();
+                tv_writing.setText(str.length() + " / 80 바이트");
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - btnBack;
+
+        if (0 <= gapTime && 2000 >= gapTime) {
+            super.onBackPressed();
+        }
+        else {
+            btnBack = curTime;
+            Toast.makeText(getApplicationContext(), "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
