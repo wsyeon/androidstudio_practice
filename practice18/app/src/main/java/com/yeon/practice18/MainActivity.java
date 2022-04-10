f5b5dd701a53a0992571d90cdc4f56f095c982d6
원성연 <sywon2005@gmail.com>
2022-04-11 05:41:27 +0900

    finsh(),backBtn

---
+++ practice18/app/src/main/java/com/yeon/practice18/MainActivity.java
@@ -0,0 +1,47 @@
+package com.yeon.practice18;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_go_out;
+    long backBtn = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_go_out = findViewById(R.id.btn_go_out);
+
+        btn_go_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // 종료 버튼 누르면 바로 나가짐
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        long curTime = System.currentTimeMillis();
+        long  gapTime = curTime - backBtn;
+
+        // 0초와 2초 사이에 한번 더 누르면 종료
+        if (0 <= gapTime && gapTime <= 2000) {
+            super.onBackPressed();
+        }
+        else {
+            backBtn = curTime;
+            Toast.makeText(getApplicationContext(), "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
