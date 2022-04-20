647dbe8e11a23e0467c7fdd642155c582644b356
원성연 <sywon2005@gmail.com>
2022-04-20 13:02:50 +0900

    동영상 재생

---
+++ Practice_VideoView_20/app/src/main/java/com/yeon/practice_videoview_20/MainActivity.java
@@ -0,0 +1,31 @@
+package com.yeon.practice_videoview_20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.midi.MidiDevice;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VideoView videoView; // 비디오 실행할 수 있 도와주는
+    private MediaController mediaController; // 재생이나 정지와 같은 미디제어 버튼부 담당
+    private String videoURL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        Uri uri = Uri.parse(videoURL);
+        videoView.setMediaController(mediaController); // 미디어 제어 버튼부 세팅
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+    }
+}
