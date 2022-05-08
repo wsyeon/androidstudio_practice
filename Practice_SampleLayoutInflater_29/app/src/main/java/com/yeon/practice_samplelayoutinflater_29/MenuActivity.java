1f1997b890ec05712441a381a9d0df96ab54dcb9
원성연 <sywon2005@gmail.com>
2022-05-08 23:48:16 +0900

    LayoutInflater2

---
+++ Practice_SampleLayoutInflater_29/app/src/main/java/com/yeon/practice_samplelayoutinflater_29/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.yeon.practice_samplelayoutinflater_29;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
