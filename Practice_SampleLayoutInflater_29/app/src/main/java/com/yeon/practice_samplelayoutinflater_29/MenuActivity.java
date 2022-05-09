5b7938921731f0b85247eece2add10c0c3323169
원성연 <sywon2005@gmail.com>
2022-05-09 22:28:25 +0900

    layoutinflater3

--- Practice_SampleLayoutInflater_29/app/src/main/java/com/yeon/practice_samplelayoutinflater_29/MenuActivity.java
+++ Practice_SampleLayoutInflater_29/app/src/main/java/com/yeon/practice_samplelayoutinflater_29/MenuActivity.java
@@ -2,13 +2,36 @@ package com.yeon.practice_samplelayoutinflater_29;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
+
+    LinearLayout container;
+    Button btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        container = findViewById(R.id.container);
+        btn_add = findViewById(R.id.btn_add);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+                CheckBox cb_agree = container.findViewById(R.id.cb_agree);
+                cb_agree.setText("로딩이되었어요.");
+            }
+        });
+
+
     }
 }
