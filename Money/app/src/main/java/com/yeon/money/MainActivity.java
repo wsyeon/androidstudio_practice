ae041710a1fd4c1dfe0115b29ca4b36181eb4d95
원성연 <sywon2005@gmail.com>
2022-05-12 23:52:48 +0900

    가계부

--- Money/app/src/main/java/com/yeon/money/MainActivity.java
+++ Money/app/src/main/java/com/yeon/money/MainActivity.java
@@ -3,8 +3,11 @@ package com.yeon.money;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_out, btn_in, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
