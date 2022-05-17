5ff143b9f707d48f4fa8e8fb0270d8dc13e6630d
원성연 <sywon2005@gmail.com>
2022-05-17 23:50:41 +0900

    Intent

--- Money/app/src/main/java/com/yeon/money/MainActivity.java
+++ Money/app/src/main/java/com/yeon/money/MainActivity.java
@@ -3,15 +3,39 @@ package com.yeon.money;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_out, btn_in, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1, btn_add_1;
+    Button btn_out, btn_in, btn_add_1, btn_add_2, btn_add_3, btn_add_4, btn_add_5, btn_add_6, btn_add_7, btn_add_8, btn_add_9, btn_add_10, btn_add_11;
+    int i = 0;
+    int j = 851792;
+    TextView tv_money;
+    EditText et_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add_1 = findViewById(R.id.btn_add_1);
+        btn_out = findViewById(R.id.btn_out);
+        btn_in = findViewById(R.id.btn_in);
+        tv_money = findViewById(R.id.tv_money);
+        et_1 = findViewById(R.id.et_1);
+
+
+        btn_add_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // i = Integer.parseInt(et_1.getText().toString());
+                j = j - Integer.parseInt(et_1.getText().toString());
+                tv_money.setText(i);
+            }
+        });
     }
 }
---
+++ Pratice_SampleIntent_31/app/src/main/java/com/yeon/pratice_sampleintent_31/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.yeon.pratice_sampleintent_31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    Button btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "mike");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+    }
+}
